7ef6d31a8b764a82f4c55f2179252dbd90a921e0
Michael Dillahunty <michaeldillahunty01@gmail.com>
2022-09-19 13:34:46 -0500

    cleared

--- hw2/mjdqw5_sgsqb4_hw2.c
+++ hw2/mjdqw5_sgsqb4_hw2.c
@@ -1,28 +1,20 @@
-#include <stdio.h>
 #include "input_error.h"
+#include <stdio.h>
+/** Compiling
+   all: queue.o elevator.o
+      gcc queue.o elevator.o
+
+   queue.o: queue.c queue.h
+      gcc -Wall -Werror -g -c queue.c
+
+   elevator.o: elevator.c
+      gcc -Wall -Werror -g -c elevator.c
+
+   clean: 
+      rm *.o *.out
+*/
 
 int main(){
    
 }
 
-int input_file(FILE*fp, char*argv[]){
-   char filename[100];
-   printf("Enter filename to be used as input: ");
-   scanf("%s", filename):
-
-   fp = fopen(filename, "wr");
-}
-
-/* 
-all: queue.o elevator.o
-	gcc queue.o elevator.o
-
-queue.o: queue.c queue.h
-	gcc -Wall -Werror -g -c queue.c
-
-elevator.o: elevator.c
-	gcc -Wall -Werror -g -c elevator.c
-
-clean: 
-	rm *.o *.out
-*/
