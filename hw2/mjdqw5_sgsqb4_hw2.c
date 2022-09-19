6310e066a16fb4bf384278151c8095ff38fc64af
Michael Dillahunty <michaeldillahunty01@gmail.com>
2022-09-19 13:31:41 -0500

    house keeping

--- hw2/mjdqw5_sgsqb4_hw2.c
+++ hw2/mjdqw5_sgsqb4_hw2.c
@@ -12,3 +12,17 @@ int input_file(FILE*fp, char*argv[]){
 
    fp = fopen(filename, "wr");
 }
+
+/* 
+all: queue.o elevator.o
+	gcc queue.o elevator.o
+
+queue.o: queue.c queue.h
+	gcc -Wall -Werror -g -c queue.c
+
+elevator.o: elevator.c
+	gcc -Wall -Werror -g -c elevator.c
+
+clean: 
+	rm *.o *.out
+*/
