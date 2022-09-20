6937b433b779b38ddc224f43bcc342cc4498ba7b
Michael Dillahunty <michaeldillahunty01@gmail.com>
2022-09-20 12:26:24 -0500

    more functions

--- hw2/mjdqw5_sgsqb4_hw2.c
+++ hw2/mjdqw5_sgsqb4_hw2.c
@@ -1,13 +1,18 @@
 #include "input_error.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define INT_MAX 2147483647
 
 int getLength(FILE* fp);
 char* allocate_array(int size);
-char* read_file(FILE*fp, char* file_name);
-/* macros to test against an invalid character */
-// #define IS_UPPER_LETTER(x) (x >= 'a' && x <= 'z')
-// #define IS_LOWER_LETTER(x) (x >= 'A' && x <= 'Z')
+char* read_file(FILE*fp, char* file_name, int*arr_size);
+int partition(int arr[], int left, int right, int x);
+int xth_smallest(int arr[], int left, int right, int x);
+int getMedian(int arr[], int n);
+int compare(int a,int b);
+void swap(int*a, int*b);
+int string_to_int(char *str, int *converted_value);
+int* convert_array(char* cArr, int size);
 /** Compiling
    all: queue.o elevator.o
       gcc queue.o elevator.o
@@ -45,20 +50,26 @@ char* read_file(FILE*fp, char* file_name);
     PARSING_ERROR_EMPTY_FILE,
    };
 */
-
+//driver code
  int main(int argc, char*argv[]){
     
    FILE*fp = NULL; 
-   
+   int* size = NULL;
+   int* intArr;
+
    printf("enter a file name with the extension: \n");
    if(argc != 1){
       exit(INCORRECT_NUMBER_OF_COMMAND_LINE_ARGUMENTS);
    }
-   char*data = read_file(fp, argv[0]);
-   data++;
+   char*data_char = read_file(fp, argv[0], size);
+   intArr = convert_array(data_char,*size);
+   // int converted[];
+   //int convert[] = string_to_int(data, &converted);
+   //data++;
+   printf("xth smallest is %d", xth_smallest(intArr, 0, *size-1, intArr[0]));
  }
 
-char* read_file(FILE*fp, char* file_name){
+char* read_file(FILE*fp, char* file_name, int* arr_size){
    char *tempArray = NULL;
    scanf("%s", file_name);
    fp = fopen(file_name, "r");
@@ -68,6 +79,7 @@ char* read_file(FILE*fp, char* file_name){
    }
    
    int size = getLength(fp);
+   *arr_size = size;
    tempArray = allocate_array(size);
    size_t i = 0;
    while(fscanf(fp,"%[^\0]",&tempArray[i]) != EOF){
@@ -102,4 +114,109 @@ char* allocate_array(int size){
       return temp;
    }
    return temp;
+}
+
+int partition(int arr[],int left,int right,int x){
+   int i = 0;
+   
+   while(i<right){
+      if (arr[i]==x){
+         break;
+      }
+      i++;
+   }
+   swap(&arr[i],&arr[right]);
+   
+   i = right;
+   int j = left;
+   while(j<=(right-1)){
+      if(arr[j]<=x){
+         swap(&arr[i], &arr[j]);
+         i++;
+      }
+   }
+   swap(&arr[i],&arr[right]);
+
+
+   return i;
+   
+}
+
+void swap(int*a, int*b){
+   int temp = *a;
+   *a = *b;
+   *b = temp; 
+}
+
+int xth_smallest(int arr[], int left, int right, int x){
+   if (x<0 && x<=right-1+1){
+      int num = right-+1;  // number of elements in arr
+      int median[(num+4)/5]; // split into (n+4)/5 groups
+      int i;
+      for(i=0; i<(num/5); i++){
+         median[i] = getMedian(arr+1+i*5, num%5);
+         if(i*5 < num){ // last group with < 5 elements
+            median[i] = getMedian(arr+1+i*5, num%5);
+            i++;
+         }
+      }
+      /* find median of medians recursively */
+      int median_of_medians;
+      if (i == 1){ // if median array only has one element, then don't use recursive call
+         median_of_medians = median[i-1];
+      } else {
+         median_of_medians = xth_smallest(median, 0, i-1, i/2);
+      }
+      
+      int position = partition(arr, 1, right, median_of_medians);
+      if (position-1 == x-1){ // if current position is same as x
+         return arr[position];
+      } else if (position-1 > x-1){ // else if the position is greater, recursive call for left subarr
+         return xth_smallest(arr, 1, position-1, x);
+      }
+      return xth_smallest(arr, position+1, right, x-position+1-1); // else recursive call for right subarr
+   }
+   return INT_MAX; // if x is greater than the # of elements in the arr
+}
+
+// find median of an array
+int getMedian(int arr[], int n){
+   qsort(arr, n, sizeof(int), compare());
+   return arr[n/2];
+}
+
+// comparator function for quick select
+int compare(int a,int b){
+   return a-b;
+}
+
+/* long int strtol(const char *str, char **endptr, int base)
+   str = string containing the representation of an integeral num
+   endptr = value is set by the function to the next character in str after the numerical val
+   base = must be between 2 and 36 inclusive, or special val 0 
+*/
+// https://www.ibm.com/docs/en/i/7.1?topic=lf-strtol-strtoll-convert-character-string-long-long-long-integer
+// https://www.tutorialspoint.com/c_standard_library/c_function_atoi.htm (atoi())
+int string_to_int(char *str, int *converted_value){
+   if (str && converted_value != NULL){   // first check
+      // strtol(const char*nptr, char**endptr, int base);
+      if (INT_MAX <= strtol(str, (char**)NULL, 10)){
+         return -1; 
+      } 
+      *converted_value = atoi(str);
+      return 0;
+   }
+   return 1; 
+}
+
+int* convert_array(char* cArr, int size){
+   int *res; 
+   res = malloc(sizeof(int)*size); 
+   if (res == NULL){
+      return NULL;
+   }
+   for (int i=0; i<size; i++){   
+         string_to_int(&cArr[i], res);
+   }
+   return res;
 }
