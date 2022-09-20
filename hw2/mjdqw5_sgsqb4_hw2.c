827b43b92a1965cc07446e0c71fbc28565640f74
Michael Dillahunty <michaeldillahunty01@gmail.com>
2022-09-19 19:14:32 -0500

    removed array locate function

--- hw2/mjdqw5_sgsqb4_hw2.c
+++ hw2/mjdqw5_sgsqb4_hw2.c
@@ -25,31 +25,18 @@ int readFile(FILE* fp,char* a)
    printf("input filename: ");
    scanf("%s",fileName);
    fp = fopen(fileName,'r');
-<<<<<<< HEAD
    if (fp == NULL) 
       return -1; // return -1 on file open failure
    while (fscanf(fp, "%[^\n] ", a) != EOF) {
-=======
 
 
    while (fp!= EOF) {
       fscanf(fp, "%[^\n] ", a) != EOF);
->>>>>>> 27ed452c7fe27a8c6556f10954f83197b79909ff
         
     }
-
-
-
 }
 
 
-/** Function to find the x-th smallest element
-   \param data: current values in the array
-   \param target: x-th smallest element in the array
-   \param elem_size:  the size of the array
-   \param count: number of elements in the array 
-*/
-int array_locate(void*data, void*target, int size, int count){
-   
-}
+
+
 
